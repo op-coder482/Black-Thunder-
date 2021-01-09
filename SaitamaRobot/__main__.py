cfae2b34572c647805190265a1e903eaa84019b3
Swami 482 <76770812+op-coder482@users.noreply.github.com>
2021-01-09 12:08:52 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -75,11 +75,9 @@ def get_readable_time(seconds: int) -> str:
 
 PM_START_TEXT = """
 Hi {}, my name is {}! 
-I am an Anime themed group management bot.
-Build by weebs for weebs, I specialize in managing anime and similar themed groups.
-You can find my list of available commands with /help.
+I am Black lighting, as fast as lightning a superb telegram group managing bot helping your group to keep safe and clean 💫💫.
+Type /help to see all the available modules loaded by my master 👀....
 """
-
 HELP_STRINGS = """
 Hey there! My name is *{}*.
 I'm a Hero For Fun and help admins manage their groups with One Punch! Have a look at the following for an idea of some of \
@@ -246,7 +244,7 @@ def start(update: Update, context: CallbackContext):
                     [
                         InlineKeyboardButton(
                             text="🧾 Getting started guide",
-                            url="https://t.me/OnePunchUpdates/29",
+                            url="https://t.me/blackthunder1_support/13",
                         )
                     ],
                     [
